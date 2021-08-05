3f5da93bce3828fea90893764b48725e3b0287da
Tim Warner <timothywarner316@gmail.com>
2021-08-05 14:51:48 -0500

    Add Linux CSE

--- vms/windows-cse.ps1
+++ vms/windows-cse.ps1
@@ -1,3 +1,9 @@
+# Set the time zone
+Set-TimeZone -Name "Central Standard Time"
+
+# Disable Firewall (careful!)
+Set-NetFirewallProfile -Profile Domain, Public, Private -Enabled False
+
 # Install IIS
 Install-WindowsFeature -Name Web-Server -IncludeManagementTools
 
@@ -6,3 +12,13 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object
 
 #Install Software
 choco install microsoft-edge powershell-core azurepowershell azure-cli bicep vscode -y
+
+# Disable IE Enhanced Security Config
+function Disable-IEESC {
+  $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+  $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+  Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+  Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+  Stop-Process -Name Explorer
+}
+Disable-IEESC
